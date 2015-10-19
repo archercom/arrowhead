451006c48d3555d7e5d9a1b24aa9feaadf7cb3d0
luis rosario <lrosario101@gmail.com>
2015-10-18 23:01:16 -0400

    new spacing

--- js/arrowhead/sample.js
+++ js/arrowhead/sample.js
@@ -1,19 +1,19 @@
 (function ($, window, document, undefined) {
-    'use strict';
+  'use strict';
 
-    /**
-     * Sample - a sample JS module file for our project
-     */
+  /**
+   * Sample - a sample JS module file for our project
+   */
 
-    strath.modules.sample = {
+  strath.modules.sample = {
 
-        init: function() {
-            console.log('hello from: sample.js');
-            // every module should have an init function
-            // the init() should have a test built-in if the JS is designed to
-            // run on a certain page
-        }
+    init: function() {
+      console.log('hello from: sample.js');
+      // every module should have an init function
+      // the init() should have a test built-in if the JS is designed to
+      // run on a certain page
+    }
 
-    };
+  };
 
 }($ || jQuery, window, window.document));
